9d22c56cd1f212cc61eb320bf1f21b3192a9cb26
alexcapoluongo <alexcapoluongo@icloud.com>
2022-04-27 14:00:55 +0200

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+//creare 5 numeri random da 1 a 99 
+
+
+// crea un timer di 30 secondi che parte appena si visualizza la pagina
+
+//software dice quanti numeri hai indovinato e quali
