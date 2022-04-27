81cbb3d6ff79e3330d04f3ff17debfcacbd8aaa9
alexcapoluongo <alexcapoluongo@icloud.com>
2022-04-27 14:24:33 +0200

    added function to generate 5 numbers

--- js/script.js
+++ js/script.js
@@ -4,6 +4,26 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto pr
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
 //creare 5 numeri random da 1 a 99 
+// - creare un ciclo for per generare i numeri
+// -creare un array vuoto dove andranno 5 numeri (do while)
+
+generateFiveNumbers();
+
+function generateFiveNumbers() {
+    fiveNumbers = [];
+    for (let i=0; i<5; i++) {
+        //crea un numero random
+        const numbers = Math.floor(Math.random()*100);
+        console.log(numbers);
+        //pushato i 5 numeri random nell'array
+        fiveNumbers.push(numbers);
+    }
+    console.log(fiveNumbers);
+    //stampa i numeri
+    document.getElementById('numbers').innerHTML = fiveNumbers;
+}
+
+
 
 
 // crea un timer di 30 secondi che parte appena si visualizza la pagina
