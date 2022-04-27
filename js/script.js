040bac8c650595b38313ce2ce5db65658d1e1b95
alexcapoluongo <alexcapoluongo@icloud.com>
2022-04-27 15:14:12 +0200

    trial print right numbers

--- js/script.js
+++ js/script.js
@@ -3,19 +3,27 @@ Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
-//creare 5 numeri random da 1 a 99 
-// - creare un ciclo for per generare i numeri
-// -creare un array vuoto dove andranno 5 numeri (do while)
+
 
 generateFiveNumbers();
 
+//crea un array vuoto
+//crea un ciclo for da 1 a 5 
+// - crea una variabile per numeri random da 1 a 100
+// - pusha i numeri random nell'array
+// stampa 5 numeri dell array nel dom
+
+/**
+ * Description: this function generate 5 random numbers and print them on DOM
+ * @returns void
+ */
 function generateFiveNumbers() {
     fiveNumbers = [];
     for (let i=0; i<5; i++) {
         //crea un numero random
         const numbers = Math.floor(Math.random()*100);
         console.log(numbers);
-        //pushato i 5 numeri random nell'array
+        //push i 5 numeri random nell'array
         fiveNumbers.push(numbers);
     }
     console.log(fiveNumbers);
@@ -24,8 +32,53 @@ function generateFiveNumbers() {
 }
 
 
-
-
 // crea un timer di 30 secondi che parte appena si visualizza la pagina
+// - creare una variabile seconds
+const seconds = 5 * 1000;
+
+// -fai partire il timer
+setTimeout(hidNumbers, seconds);;
+
+// - creare una funzione che ti dirà cosa succede dopo i 30 secondi 
+//     + scompare l'array in html
+
+/**
+ * Description: this function hid the numbers on DOM
+ * @returns void
+ */
+function hidNumbers () {
+    let numbers = document.getElementById('numbers');
+    numbers.classList.add('hidden');
+}
+
+//     + 5 prompt, un secondo di ritardo rispetto a writeNumbers() per non mostrare i numeri dietro
+
+setTimeout(askNumbers, 6000);
+
+/**
+ * Description: this function ask five prompts to ask numbers. then print on dom which number the user guessed
+ * @returns: void
+ */
+
+function askNumbers() {
+    for (let i = 0; i < 5; i++) {
+        // chiedi 5 prompt
+        const userNumbers = parseInt(prompt('scrivi un numero'));
+        // se il numero scritto è presente nel dom 
+        // - true
+        // - stampa in dom
+        // altrimenti
+        // - il numero non viene stampato
+        if (userNumbers === fiveNumbers[i]) {  
+            let choice = true;
+            console.log(choice);
+        } else {
+            let choice = false;
+            console.log(choice);
+        }
+    }
+}
 
 //software dice quanti numeri hai indovinato e quali
+
+
